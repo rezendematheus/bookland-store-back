93563e745d9d8b5373b5b7a1835053ae1333e819
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2023-01-24 21:25:49 -0300

    feat: database.js

---
+++ src/database/database.js
@@ -0,0 +1,17 @@
+import { MongoClient, MongoCompatibilityError } from "mongodb";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+
+let db;
+
+try {
+    await mongoClient.connect()
+    const db = mongoClient.db("bookland-database")
+} catch (error) {
+    
+}
+
+export default db
