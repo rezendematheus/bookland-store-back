8410f4739448f4afd511799ebf9d5851f647cb0d
Matheus Rezende <matheusreisrezende.2002@gmail.com>
2023-01-25 13:34:36 -0300

    feat: middleware schemaValidation

---
+++ src/middlewares/schemaValidation.middleware.js
@@ -0,0 +1,16 @@
+
+
+export const schemaValidation = (schema) => {
+    return (req, res, next) => {
+        const { error } = schema
+            .validate(req.body, { abortEarly: false })
+
+        if (error) {
+            const errorDetails = error.details.map(detail => detail.message)
+            return res.status(422).send(errorDetails)
+        }
+
+        next();
+    }
+
+}
